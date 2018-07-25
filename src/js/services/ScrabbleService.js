90a27e4fac5b9db71f16db38db9be92f216abd1f
Tom Fitton <thomas.fitton1991@gmail.com>
2018-07-25 20:24:42 +0100

    Add trivial and crude array of possible square types

--- src/js/services/ScrabbleService.js
+++ src/js/services/ScrabbleService.js
@@ -1,5 +1,32 @@
 window.ScrabblePro.service("ScrabbleService", [function() {
 
+  var cellTypes = [
+    {
+      "id": 1,
+      "descriptor": "BLANK"
+    },
+    {
+      "id": 2,
+      "descriptor": "START"
+    },
+    {
+      "id": 3,
+      "descriptor": "LETTER_DOUBLE"
+    },
+    {
+      "id": 4,
+      "descriptor": "LETTER_TRIPLE"
+    },
+    {
+      "id": 5,
+      "descriptor": "WORD_DOUBLE"
+    },
+    {
+      "id": 6,
+      "descriptor": "WORD_TRIPLE"
+    }
+  ];
+
   this.getMessage = function() {
     return "hello from service";
   };

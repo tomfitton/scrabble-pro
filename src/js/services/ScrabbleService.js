2103ab45e491fb34af0cb0b60f9092a71181e413
Tom Fitton <thomas.fitton1991@gmail.com>
2018-07-25 20:31:21 +0100

    Add board creation scaffolding

--- src/js/services/ScrabbleService.js
+++ src/js/services/ScrabbleService.js
@@ -1,34 +1,96 @@
 window.ScrabblePro.service("ScrabbleService", [function() {
 
-  var cellTypes = [
-    {
-      "id": 1,
-      "descriptor": "BLANK"
-    },
-    {
-      "id": 2,
-      "descriptor": "START"
-    },
-    {
-      "id": 3,
-      "descriptor": "LETTER_DOUBLE"
-    },
-    {
-      "id": 4,
-      "descriptor": "LETTER_TRIPLE"
-    },
-    {
-      "id": 5,
-      "descriptor": "WORD_DOUBLE"
-    },
-    {
-      "id": 6,
-      "descriptor": "WORD_TRIPLE"
-    }
-  ];
+  var cellTypes = {
+    "BLANK": 0,
+    "START": 1,
+    "LETTER_DOUBLE": 2,
+    "LETTER_TRIPLE": 3,
+    "WORD_DOUBLE": 4,
+    "WORD_TRIPLE": 5
+  };
 
   this.getMessage = function() {
     return "hello from service";
+  };
+
+  function getRowA() {
+
+  }
+
+  function getRowB() {
+
+  }
+
+  function getRowC() {
+
+  }
+
+  function getRowD() {
+
+  }
+
+  function getRowE() {
+
+  }
+
+  function getRowF() {
+
+  }
+
+  function getRowG() {
+
+  }
+
+  function getRowH() {
+
+  }
+
+  function getRowI() {
+
+  }
+
+  function getRowJ() {
+
+  }
+
+  function getRowK() {
+
+  }
+
+  function getRowL() {
+
+  }
+
+  function getRowM() {
+
+  }
+
+  function getRowN() {
+
+  }
+
+  function getRowO() {
+
+  }
+
+  this.getBoard = function() {
+    var board = [];
+    board.push(getRowA());
+    board.push(getRowB());
+    board.push(getRowC());
+    board.push(getRowD());
+    board.push(getRowE());
+    board.push(getRowF());
+    board.push(getRowG());
+    board.push(getRowH());
+    board.push(getRowI());
+    board.push(getRowJ());
+    board.push(getRowK());
+    board.push(getRowL());
+    board.push(getRowM());
+    board.push(getRowN());
+    board.push(getRowO());
+    return board;
   };
 
 }]);

b757f1c45b3fa6198663ef982bbea6dc5171e71f
Tom Fitton <thomas.fitton1991@gmail.com>
2018-07-25 23:07:23 +0100

    Update

--- src/js/controllers/Application.js
+++ src/js/controllers/Application.js
@@ -2,4 +2,25 @@ window.ScrabblePro.controller("ApplicationController", ["$scope", "ScrabbleServi
 
   $scope.board = ScrabbleService.getBoard();
 
+  $scope.computeClass = function(value) {
+    if (value == 0) {
+      return "blank";
+    }
+    else if (value == 1) {
+      return "start";
+    }
+    else if (value == 2) {
+      return "letter-double";
+    }
+    else if (value == 3) {
+      return "letter-treble";
+    }
+    else if (value == 4) {
+      return "word-double";
+    }
+    else if (value == 5) {
+      return "word-treble";
+    }
+  };
+
 }]);
